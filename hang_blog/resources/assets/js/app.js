642fa00813994d0eabf4d5b570c5a5a44330a962
Hang Phung Thi <hang_phung@hangunoMacBook-puro.local>
2018-06-14 00:22:13 +0900

    router module

--- hang_blog/resources/assets/js/app.js
+++ hang_blog/resources/assets/js/app.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 import App from './App.vue';
 import store from './store';
+import router from './router';
 
 Vue.use(VueAxios, axios);
 
