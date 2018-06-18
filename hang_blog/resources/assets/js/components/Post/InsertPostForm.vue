c6ae610d937a309291721619ab33af4070156537
phunghangbk <phunghangbk@gmail.com>
2018-06-18 19:01:21 +0900

    vue qill editor component

--- hang_blog/resources/assets/js/components/Post/InsertPostForm.vue
+++ hang_blog/resources/assets/js/components/Post/InsertPostForm.vue
@@ -1,41 +1,98 @@
 <template>
   <div class="container-fluid custom-container">
-    <link href="/css/editor.css" type="text/css" rel="stylesheet"/>
     <div class="row justify-content-center">
-      <div class="container">
-          <div class="row">
-            <div class="col-lg-12 nopadding">
-              <textarea id="txtEditor"></textarea> 
-            </div>
-          </div>
-        </div>
+      <!-- bidirectional data binding（双向数据绑定） -->
+      <quill-editor class="col-lg-8" v-model="content"
+                    ref="myQuillEditor"
+                    :options="editorOption"
+                    @blur="onEditorBlur($event)"
+                    @focus="onEditorFocus($event)"
+                    @ready="onEditorReady($event)">
+      </quill-editor>
     </div>
   </div>
 </template>
 
 <script>
-  var editor = require("../../custom_js/editor.js");
-  export default {
-    data() {
-      return {
+  import 'quill/dist/quill.core.css'
+  import 'quill/dist/quill.snow.css'
+  import 'quill/dist/quill.bubble.css'
+  import { quillEditor } from 'vue-quill-editor'
 
+  export default {
+    components: {
+      quillEditor
+    },
+    data () {
+      return {
+        content: `<h2 class="ql-align-center"><span class="ql-font-serif">Text content loading..</span></h2>`,
+        editorOption: {
+          modules: {
+            toolbar: [
+              ['bold', 'italic', 'underline', 'strike'],
+              ['blockquote', 'code-block'],
+              [{ 'header': 1 }, { 'header': 2 }],
+              [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+              [{ 'script': 'sub' }, { 'script': 'super' }],
+              [{ 'indent': '-1' }, { 'indent': '+1' }],
+              [{ 'direction': 'rtl' }],
+              [{ 'size': ['small', false, 'large', 'huge'] }],
+              [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+              [{ 'font': [] }],
+              [{ 'color': [] }, { 'background': [] }],
+              [{ 'align': [] }],
+              ['clean'],
+              ['link', 'image', 'video']
+            ],
+          }
+        }
+      }
+    },
+
+    methods: {
+      onEditorBlur(quill) {
+        console.log('editor blur!', quill)
+      },
+      onEditorFocus(quill) {
+        console.log('editor focus!', quill)
+      },
+      onEditorReady(quill) {
+        console.log('editor ready!', quill)
+      },
+      onEditorChange({ quill, html, text }) {
+        console.log('editor change!', quill, html, text)
+        this.content = html
+      }
+    },
+    computed: {
+      editor() {
+        return this.$refs.myQuillEditor.quill
       }
     },
     mounted() {
-      this.editor();
-    },
-    methods: {
-      editor() {
-        console.log($("#txtEditor"));
-        $("#txtEditor").Editor();
-      }
+      console.log('this is current quill instance object', this.editor)
     }
   }
 </script>
 
 <style scoped>
-
   .custom-container {
     margin-top: 70px;
+  }
+
+  .quill-code {
+    border: none;
+    height: auto;
+    > code {
+      width: 100%;
+      margin: 0;
+      padding: 1rem;
+      border: 1px solid #ccc;
+      border-top: none;
+      border-radius: 0;
+      height: 10rem;
+      overflow-y: auto;
+      resize: vertical;
+    }
   }
 </style>
